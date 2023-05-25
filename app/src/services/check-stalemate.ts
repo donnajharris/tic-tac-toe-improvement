32aae373edbd4bfdbc56d5903d7a038e5e01ea7d
Donna Harris <donnajharris@gmail.com>
2023-05-25 13:18:00 -0400

    removed old check-stalemate files

--- app/src/services/check-stalemate.ts
+++
@@ -1,6 +0,0 @@
-import { Square } from '../types/square';
-
-export default function checkStalemate(squares: Square[]): boolean {
-  const unplayed = squares.filter(s => s.value === undefined);
-  return unplayed.length === 0;
-}
