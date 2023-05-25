8ff8556d5e821e590226269c2b38642be3c210d2
Donna J. Harris <donnajharris@gmail.com>
2023-05-25 01:30:33 -0400

    Revert "Fixing #2 (winning using max turns)"

--- app/src/App.vue
+++ app/src/App.vue
@@ -25,7 +25,7 @@ import NewGame from './components/NewGame.vue';
 import LeaderBoard from './components/LeaderBoard.vue';
 import { getScores, updateScore, clearScores } from './data/scores';
 import checkWin from './services/check-win';
-import checkAllTurnsPlayed from './services/check-all-turns-played';
+import checkStalemate from './services/check-stalemate';
 import { getSquareById } from './services/get-square-by-id';
 import scoreToList from './services/score-to-list';
 import { Player, Winner } from './types/player';
@@ -97,12 +97,11 @@ export default defineComponent({
           }
         });
         this.updateScore(this.turn as Winner);
-      } else {
-        const stalemate: boolean = checkAllTurnsPlayed(this.squares);
-        if (stalemate) {
-          this.gameOver = true;
-          this.updateScore('tie');
-        }
+      }
+      const stalemate: boolean = checkStalemate(this.squares);
+      if (stalemate) {
+        this.gameOver = true;
+        this.updateScore('tie');
       }
     },
 
@@ -148,31 +147,26 @@ export default defineComponent({
   color: #2c3e50;
   margin-top: 60px;
 }
-
 .flex {
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
 }
-
 .board {
   width: 613px;
   margin: auto;
 }
-
 .flex-spread {
   display: flex;
   flex-direction: row;
   flex-wrap: no-wrap;
   justify-content: space-between;
 }
-
 .footer {
   width: 613px;
   margin: auto;
 }
-
-.footer>* {
+.footer > * {
   width: 49%;
 }
 </style>
